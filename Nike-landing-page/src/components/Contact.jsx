e84446ec0fd3848c1c786e2208dedd9818db856a
itmadityajoshi <adityajoshi316@gmail.com>
2024-08-15 20:14:38 +0545

    contact page created

--- Nike-landing-page/src/components/Contact.jsx
+++ Nike-landing-page/src/components/Contact.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='text-4xl'>Contact</div>
+    <div className='container  mx-16'>
+
+        <h1 className='font-bold text-3xl mt-10 mb-8'>CONTACT US</h1>
+        <p className='text-sm text-gray-900 w-[820px]'>LET’S CONNECT: WE’RE HERE TO HELP, AND WE’D LOVE TO HEAR FROM YOU! WHETHER YOU HAVE A QUESTION, COMMENT, OR JUST WANT TO CHAT , YOU CAN REACH OUT TO US THROUGH THE CONTACT FORM OF THIS PAGE, OR BY PHONE, EMAIL, OR SOCIAL MEDIA. </p>
+
+        <div className=''>
+            <img src="/public/photos/Frame.png" alt="" srcset="" />
+        </div>
+    </div>
   )
 }
 
