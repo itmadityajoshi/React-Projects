a882f56feddbfe6f6a780691cf2938295acec9bf
itmadityajoshi <adityajoshi316@gmail.com>
2024-08-19 00:06:28 +0545

    input text and length label desinged

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -17,13 +17,39 @@ const App = () => {
 
     for (let i = 0; i < array.length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      
+      pass = str.charAt(char)
     }
+
+    setPassword(pass)
 
   }, [length, numberAllowed, characterAllowed, setPassword])
   return (
     <>
-      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+      
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+      <h1 className="text-2xl text-center text-white mb-3 my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 ">
+          <input 
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="password"
+          readOnly />
+
+          <button className=" border-orange-500 border  px-3 py-0.5 shrink-0 outline-none">Copy</button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="text"
+            min={8}
+            max={25}
+            value={length}
+            className="cursor-pointer" />
+            <label>Length:{length}</label>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
