3bc3b3e79f2ff47efc8dbf84bea0fb0961fce7df
itmadityajoshi <adityajoshi316@gmail.com>
2024-08-22 22:55:19 +0545

    learning crud in reactjs

---
+++ crud-noteapp/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AddTask from './components/AddTask'
+
+const App = () => {
+  return (
+    <>
+      <AddTask/>
+    </>
+  )
+}
+
+export default App
---
+++ crud-noteapp/src/components/AddTask.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+
+const AddTask = () => {
+  const [task, setTask] = useState("");
+  const [day, setDay] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!task && !day && !desc) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops..",
+        text: "Fill in your all your data.",
+        timer: 4000,
+      });
+    } else if (!task && !desc && day) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill in your Task and Description data.",
+      });
+    } else if (!task && desc && day) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill in your task data.",
+      });
+    } else if (task && !desc && day) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Fill in your Descriptions data.",
+      });
+    } else {
+      onSave({ task, desc, day });
+    }
+
+    setTask("");
+    setDesc("");
+    setDay("");
+  };
+
+  const onSave= ()=>{
+    
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="grid tems-center justify-center gap-6 my-8  text-xl mx-auto grid-rows-3"
+    >
+      <div className="">
+        <span>Add Task</span>
+        <input
+          type="text"
+          placeholder="Type Task here"
+          className="input input-bordered w-full max-w-xs"
+          value={task}
+          onChange={(e) => e.target.value}
+        />
+      </div>
+      <div className="">
+        <span>Add Descriptions</span>
+        <input
+          type="text"
+          placeholder="Type descriptions here "
+          className="input input-bordered w-full max-w-xs"
+          value={desc}
+          onChange={(e) => e.target.value}
+        />
+      </div>
+
+      <div className="">
+        <span>Add Date and Time</span>
+        <input
+          type="text"
+          placeholder="Type Date and Time here "
+          className="input input-bordered w-full max-w-xs"
+          value={day}
+          onChange={(e) => e.target.value}
+        />
+      </div>
+
+      <button className="btn" value={onSave}>
+        Save Task
+      </button>
+    </form>
+  );
+};
+
+export default AddTask;
