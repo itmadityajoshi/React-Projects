65b4fb18271bae89f3ae191a158b6b3f0838f430
itmadityajoshi <adityajoshi316@gmail.com>
2024-08-24 22:43:39 +0545

    update component created with abbility to update users data

--- crud-noteapp/src/components/Update.jsx
+++ crud-noteapp/src/components/Update.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const Update = () => {
-  return (
-    <div>Update</div>
-  )
-}
+  //   const [data, setData] = useState([]);
 
-export default Update
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users/" + id)
+      .then((res) => {
+        setValues(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .put("http://localhost:3000/users/" +id, values)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="flex w-full mt-10 justify-center items-center">
+      <div className=" w-[50%] border bg-white shadow px-5 pt-3 pb-5 rounded">
+        <h1 className="text-3xl font-semibold mb-4">Update User</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-2">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Name :
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter Name"
+              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={values.name}
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email :
+            </label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter Email"
+              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={values.email}
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            />
+          </div>
+          <div className="mb-2">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Phone :
+            </label>
+            <input
+              type="text"
+              name="phone"
+              placeholder="Enter Phone"
+              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={values.phone}
+              onChange={(e) => setValues({ ...values, phone: e.target.value })}
+            />
+          </div>
+          <button className="bg-green-500 px-2 text-xl rounded m-2 ">
+            Submit
+          </button>
+          <Link to="/">
+            <button className="bg-gray-400 px-2 rounded text-xl"> Back </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Update;
